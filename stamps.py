589eee43d00b181ceb6d2eb3a08acaa2601030f9
tnd-viphap <nhatduongtran@viphap.com>
2025-03-01 12:11:41 +0700

    Added converting CSV to SHP feature

--- stamps.py
+++ stamps.py
@@ -25,15 +25,17 @@ class StaMPSEXE:
     def run(self):
         if self.process_type == 'NORMAL':
             try:
-                os.system(f"matlab -nojvm -nosplash{self.display} -r \"run('{os.path.split(os.path.abspath(__file__))[0]}/modules/StaMPS/autorun_normal.m'); exit;\" > {self.CURRENT_RESULT}/STAMPS.log")
+                #os.system(f"matlab -nojvm -nosplash{self.display} -r \"run('{os.path.split(os.path.abspath(__file__))[0]}/modules/StaMPS/autorun_normal.m'); exit;\" > {self.CURRENT_RESULT}/STAMPS.log")
                 time.sleep(1)
                 for patch in [os.path.join(self.CURRENT_RESULT, f) for f in os.listdir(self.CURRENT_RESULT) if f.startswith('PATCH_')]:
-                    csv_file = [f for f in os.listdir(patch) if f.endswith('.csv')]
-                    folder = patch.split('/')[-2]
+                    csv_file = [os.path.join(patch, f) for f in os.listdir(patch) if f.endswith('.csv')]
+                    folder = patch.split('/')[-1]
                     if csv_file:
                         data = gpd.read_file(csv_file[0])
-                        data.geometry = [Point(x, y) for x, y in zip(data.LON, data.LAT)]
-                        data.to_file(os.path.join(self.DATAFOLDER, f'geom/{self.CURRENT_RESULT.split("/")[-1]}_{folder}.shp'))
+                        geom = [Point(f) for f in zip(data.LON, data.LAT)]
+                        data = gpd.GeoDataFrame(data, geometry=geom, crs="EPSG:4326")
+                        outfile = os.path.join(self.DATAFOLDER, f'geom/{self.CURRENT_RESULT.split("/")[-1]}_{folder}.shp')
+                        data.to_file(outfile, driver="ESRI Shapefile")
             except:
                 sys.exit(0)    
         elif self.process_type == 'TOMO':
