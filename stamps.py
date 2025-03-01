78bac6e22d594f32804489aa04f2549191270dbc
dannytran00 <nhatduongtran@viphap.com>
2025-03-01 10:36:41 +0700

    Updated auto StaMPS

--- stamps.py
+++ stamps.py
@@ -1,5 +1,8 @@
 import os
 import sys
+import time
+import geopandas as gpd
+from shapely.geometry import Point
 
 class StaMPSEXE:
     def __init__(self, process_type=None, display=' -nodisplay'):
@@ -23,6 +26,14 @@ class StaMPSEXE:
         if self.process_type == 'NORMAL':
             try:
                 os.system(f"matlab -nojvm -nosplash{self.display} -r \"run('{os.path.split(os.path.abspath(__file__))[0]}/modules/StaMPS/autorun_normal.m'); exit;\" > {self.CURRENT_RESULT}/STAMPS.log")
+                time.sleep(1)
+                for patch in [os.path.join(self.CURRENT_RESULT, f) for f in os.listdir(self.CURRENT_RESULT) if f.startswith('PATCH_')]:
+                    csv_file = [f for f in os.listdir(patch) if f.endswith('.csv')]
+                    folder = patch.split('/')[-2]
+                    if csv_file:
+                        data = gpd.read_file(csv_file[0])
+                        data.geometry = [Point(x, y) for x, y in zip(data.LON, data.LAT)]
+                        data.to_file(os.path.join(self.DATAFOLDER, f'geom/{self.CURRENT_RESULT.split("/")[-1]}_{folder}.shp'))
             except:
                 sys.exit(0)    
         elif self.process_type == 'TOMO':
