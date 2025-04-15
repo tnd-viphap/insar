d7abc6dd77797a74281a1dd5e9c875054ab252dd
tnd-viphap <nhatduongtran@viphap.com>
2025-04-15 14:59:16 +0700

    Updated schedule logic

--- schedule.sh
+++ schedule.sh
@@ -5,7 +5,7 @@ TODAY=$(date +%s)
 if [ -f "$LAST_RUN_FILE" ]; then
     LAST_RUN=$(cat "$LAST_RUN_FILE")
     DIFF=$(( (TODAY - LAST_RUN) / 86400 ))
-    if [ $DIFF -lt 15 ]; then
+    if [ $DIFF -lt 21 ]; then
         exit 0
     fi
 fi
