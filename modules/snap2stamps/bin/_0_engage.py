a483df0570f57935830ddd2f539a1d5f9e4f318d
tnd2k <nhatduongtran@viphap.com>
2025-06-19 14:41:52 +0700

    Updated config logic

--- modules/snap2stamps/bin/_0_engage.py
+++ modules/snap2stamps/bin/_0_engage.py
@@ -3,9 +3,15 @@ import json
 import os
 import psutil
 import platform
+import sys
+
+project_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..')).replace("\\", "/")
+sys.path.append(project_path)
+
+from config.parser import ConfigParser
 
 class Initialize:
-    def __init__(self, bbox, direction, frame, max_perp=None, ptype=None, stack_size=None, unified_flag=None):
+    def __init__(self, bbox, direction, frame, max_perp=None, ptype=None, stack_size=None, unified_flag=None, project_name="default"):
         super().__init__()
         plf = platform.system()
         
@@ -16,126 +22,106 @@ class Initialize:
         self.ptype = ptype
         self.stack_size = stack_size
         self.unified_flag = unified_flag
-        # To replace
-        project_folder = os.path.split(os.path.abspath(__file__))[0].split('modules')[0]
+        self.project_name = project_name
+        
+        # Initialize config parser
+        config_path = os.path.join(project_path, "config", "config.json")
+        self.config_parser = ConfigParser(config_path)
+        self.config = self.config_parser.get_project_config(self.project_name)
+        
+        # Set up project folders
+        project_folder = os.path.join(project_path).replace("\\", "/")
         if plf == "Windows":
             project_in_disk = project_folder[0].upper()
             project_folder = f"{project_in_disk}:{project_folder.split(':')[1]}"
-        log_folder = project_folder + '/logs/'
-        datafolder = project_folder + '/data/'
-        master_folder = project_folder + '/data/master/'
-        slaves_folder = project_folder + '/data/slaves/'
-        rawdata_folder = project_folder + '/data/raw/'
-        coreg_folder = project_folder + '/process/coreg/'
-        ifg_folder = project_folder + '/process/ifg/'
-        stamp_folder = project_folder + '/results/'
-        for folder in [log_folder, datafolder, master_folder, slaves_folder, rawdata_folder,
-                       project_folder+'/process/', coreg_folder, ifg_folder, stamp_folder]:
+            
+        # Create folder structure
+        folders = {
+            'project_folder': project_folder,
+            'graphs_folder': os.path.join(project_folder, 'modules/snap2stamps/graphs/'),
+            'log_folder': os.path.join(project_folder, 'logs/'),
+            'data_folder': os.path.join(project_folder, 'data/'),
+            'master_folder': os.path.join(project_folder, 'data/master/'),
+            'master': os.path.join(project_folder, 'data/master/20200101/20200101_M.dim'),
+            'slaves_folder': os.path.join(project_folder, 'data/slaves/'),
+            'raw_data_folder': os.path.join(project_folder, 'data/raw/'),
+            'coreg_folder': os.path.join(project_folder, 'process/coreg/'),
+            'ifg_folder': os.path.join(project_folder, 'process/ifg/'),
+            'stamp_folder': os.path.join(project_folder, 'results/'),
+            'config_path': os.path.join(project_folder, 'modules/snap2stamps/bin/project.conf')
+        }
+
+        # Create directories
+        for folder in folders.values():
             os.makedirs(folder, exist_ok=True)
-        config_file = project_folder + '/modules/snap2stamps/bin/project.conf'
-        graphs_folder = config_file.replace("bin/project.conf", "graphs/")
-        lonmin = self.bbox[0]
-        latmin = self.bbox[1]
-        lonmax = self.bbox[2]
-        latmax = self.bbox[3]
-        dc = project_folder + '/data/download_cache.txt'
-        bc = project_folder + '/data/broken_cache.txt'
-        bsc = project_folder + '/data/baseline_cache.txt'
-        datalake = project_folder + '/data/lake.json'
-        for file in [dc, bc, bsc]:
-            if not os.path.exists(file):
-                with open(file, 'w') as f:
-                    f.close()
+            
+        # Set up cache files
+        cache_files = {
+            'download_cache': os.path.join(project_folder, 'data/download_cache.txt'),
+            'broken_cache': os.path.join(project_folder, 'data/broken_cache.txt'), 
+            'baseline_cache': os.path.join(project_folder, 'data/baseline_cache.txt')
+        }
+
+        # Create empty cache files if they don't exist
+        for cache_file in cache_files.values():
+            if not os.path.exists(cache_file):
+                with open(cache_file, 'w') as f:
+                    pass
+
+        # Set up datalake
+        datalake = os.path.join(project_folder, 'data/lake.json')
         if not os.path.exists(datalake):
             with open(datalake, 'w') as f:
                 json.dump([], f, indent=4)
-                f.close()
+
+        # Set up computing resources
         n_cores = round(os.cpu_count() * 0.8)
         total_ram = round(psutil.virtual_memory().total / (1024 ** 3) * 0.8)
+        
+        # Set up GPT path
         if plf == "Windows":
             gpt = "C:/Program Files/snap/bin/gpt.exe"
-        elif plf == "Linux":
-            gpt = os.path.split(os.path.abspath(__file__))[0].split('insar')[0] + 'snap/bin/gpt'
-        
-        self.update_config(config_file, [project_folder, graphs_folder, log_folder,
-                                    master_folder, slaves_folder, rawdata_folder,
-                                    coreg_folder, ifg_folder, stamp_folder,
-                                    config_file,
-                                    lonmin, latmin, lonmax, latmax,
-                                    dc, bc, bsc, datalake, self.direction, self.frame, datafolder, self.ptype,
-                                    self.stack_size, self.unified_flag,
-                                    gpt, n_cores, total_ram, self.max_perp])
-        
-    # Update config
-    def update_config(self, config_file, path):
-        lines = ''''''
-        with open(config_file, 'r') as file:
-            lines = file.readlines()
-            for idx, line in enumerate(lines):
-                if line.startswith("PROJECTFOLDER"):
-                    lines[idx] = "PROJECTFOLDER=" + str(path[0]).replace('\\', '/').replace('//', '/') + '\n'
-                elif line.startswith("GRAPHSFOLDER"):
-                    lines[idx] = "GRAPHSFOLDER=" + str(path[1]).replace('\\', '/').replace('//', '/') + '\n'
-                elif line.startswith("LOGFOLDER"):
-                    lines[idx] = "LOGFOLDER=" + str(path[2]).replace('\\', '/').replace('//', '/') + '\n'
-                elif line.startswith("MASTERFOLDER"):
-                    lines[idx] = "MASTERFOLDER=" + str(path[3]).replace('\\', '/').replace('//', '/') + '\n'
-                elif line.startswith("SLAVESFOLDER"):
-                    lines[idx] = "SLAVESFOLDER=" + str(path[4]).replace('\\', '/').replace('//', '/') + '\n'
-                elif line.startswith("RAWDATAFOLDER"):
-                    lines[idx] = "RAWDATAFOLDER=" + str(path[5]).replace('\\', '/').replace('//', '/') + '\n'
-                elif line.startswith("COREGFOLDER"):
-                    lines[idx] = "COREGFOLDER=" + str(path[6]).replace('\\', '/').replace('//', '/') + '\n'
-                elif line.startswith("IFGFOLDER"):
-                    lines[idx] = "IFGFOLDER=" + str(path[7]).replace('\\', '/').replace('//', '/') + '\n'
-                elif line.startswith("STAMPFOLDER"):
-                    lines[idx] = "STAMPFOLDER=" + str(path[8]).replace('\\', '/').replace('//', '/') + '\n'
-                elif line.startswith("CONFIG_PATH"):
-                    lines[idx] = "CONFIG_PATH=" + str(path[9]).replace('\\', '/').replace('//', '/') + '\n'
-                elif line.startswith("LONMIN"):
-                    lines[idx] = "LONMIN=" + str(path[10]) + '\n'
-                elif line.startswith("LATMIN"):
-                    lines[idx] = "LATMIN=" + str(path[11]) + '\n'
-                elif line.startswith("LONMAX"):
-                    lines[idx] = "LONMAX=" + str(path[12]) + '\n'
-                elif line.startswith("LATMAX"):
-                    lines[idx] = "LATMAX=" + str(path[13]) + '\n'
-                elif line.startswith("DOWNLOAD_CACHE"):
-                    lines[idx] = "DOWNLOAD_CACHE=" + str(path[14]).replace('\\', '/').replace('//', '/') + '\n'
-                elif line.startswith("BROKEN_CACHE"):
-                    lines[idx] = "BROKEN_CACHE=" + str(path[15]).replace('\\', '/').replace('//', '/') + '\n'
-                elif line.startswith("BASELINE_CACHE"):
-                    lines[idx] = "BASELINE_CACHE=" + str(path[16]).replace('\\', '/').replace('//', '/') + '\n'
-                elif line.startswith("DATALAKE"):
-                    lines[idx] = "DATALAKE=" + str(path[17]).replace('\\', '/').replace('//', '/') + '\n'
-                elif line.startswith("DIRECTION"):
-                    lines[idx] = "DIRECTION=" + str(path[18]) + '\n'
-                elif line.startswith("FRAME"):
-                    lines[idx] = "FRAME=" + str(path[19]) + '\n'
-                elif line.startswith("DATAFOLDER"):
-                    lines[idx] = "DATAFOLDER=" + str(path[20]).replace('\\', '/').replace('//', '/') + '\n'
-                elif line.startswith("COMSAR"):
-                    lines[idx] = "COMSAR=" + str(path[21]) + '\n'
-                elif line.startswith("MINISTACK"):
-                    lines[idx] = "MINISTACK=" + str(path[22]) + '\n'
-                elif line.startswith("UNIFIED"):
-                    lines[idx] = "UNIFIED=" + str(path[23]) + '\n'
-                elif line.startswith("GPTBIN_PATH"):
-                    lines[idx] = "GPTBIN_PATH=" + str(path[24]).replace(str(path[2][0]), str(path[2][0])) + '\n'
-                elif line.startswith("CPU"):
-                    lines[idx] = "CPU=" + str(path[25]) + '\n'
-                elif line.startswith("CACHE"):
-                    lines[idx] = "CACHE=" + str(path[26]) + 'G\n'
-                elif line.startswith("MAX_PERP"):
-                    lines[idx] = "MAX_PERP=" + str(path[27]) + '\n'
-            
-        with open(config_file, "w") as file:
-            file.writelines(lines)
-            file.close()
+        else:
+            gpt = "/home/viphap/snap9/bin/gpt"
+
+        # Update config dictionary
+        config_update = {
+            "project_definition": folders,
+            "cache_files": cache_files,
+            "search_parameters": {
+                "datalake": datalake,
+                "direction": self.direction,
+                "frame": self.frame
+            },
+            "processing_parameters": {
+                "current_result": folders['stamp_folder']+'/INSAR_20200101_PSDS_v0/',
+                "max_perp": self.max_perp,
+                "ptype": self.ptype,
+                "ministack": self.stack_size,
+                "unified": self.unified_flag
+            },
+            "aoi_bbox": {
+                "lon_min": self.bbox[0],
+                "lat_min": self.bbox[1], 
+                "lon_max": self.bbox[2],
+                "lat_max": self.bbox[3]
+            },
+            "snap_gpt": {
+                "gptbin_path": gpt
+            },
+            "computing_resources": {
+                "cpu": n_cores,
+                "cache": f"{total_ram}G"
+            }
+        }
+
+        # Update config file
+        self.config.update(config_update)
+        self.config_parser.update_project_config(self.project_name, self.config)
 
 if __name__ == "__main__":
     try:
         bbox = [106.6969, 10.7615, 106.7275, 10.7945]
-        Initialize(bbox)
+        Initialize(bbox, "DESCENDING", 522, project_name="default")
     except Exception as e:
         print(f"Engage project structure fails due to\n{e}\n")
